8a2aa4ed62e807d24815be05c73de224debb76b7
Nathalie <nat933@free.fr>
2025-08-06 13:52:40 +0200

    style: ajuster la configuration TypeScript et le formatage des fichiers pour une meilleure lisibilité

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-    
-    "baseUrl": ".",
-    "paths": {
+       
       "@/*": ["src/*"]
-    
-  }
+      
 };
 
 export default nextConfig;
