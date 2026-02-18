6f2d4749d5d6a28afe4e32f7cba102dbdf23acf8
Nathalie <nat933@free.fr>
2026-02-18 13:31:54 +0100

    bonne syntaxe pour  setup (Next 16 + next-auth v5)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/app/lib/authOptions";
 
-const handler = NextAuth(authOptions);
+export const { handlers } = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const GET = handlers.GET;
+export const POST = handlers.POST;
